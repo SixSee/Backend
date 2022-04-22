cd1f6c4eacc9ae8946821dfe84bd30c11e74ab41
Kunwar Pratap Singh <ksingh2001.dev@gmail.com>
2022-04-22 20:58:08 +0530

    Update env.py

--- env.py
+++ env.py
@@ -1,2 +1,11 @@
 DEBUG=True
 SECRET="your_secret_key"
+GOOGLE_CLIENT_ID = "google_client_id"
+GOOGLE_CLIENT_SECRET = "google_client_secret"
+CURRENT_ENV = 'development' # if you want to use local database set this variable to 'local'
+
+DB_USER = 'postgres'
+DB_NAME = 'postgres'
+DB_PASSWORD = 'postgres'
+DB_HOST = 'localhost'
+DB_PORT = '5432'
