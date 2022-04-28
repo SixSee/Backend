962fcbd072287a3952ec5fc534f27f7e35dc58b3
ToxicityMax <ksingh2001.dev@gmail.com>
2022-04-29 00:52:50 +0530

    Approve api for bulletin

--- apps/bulletin/urls.py
+++ apps/bulletin/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path, include
-from .views import (BulletinView)
+from django.urls import path
+
+from .views import (BulletinView, SingleBulletinView, ApproveBulletinView)
 
 urlpatterns = [
     path('', BulletinView.as_view()),
-    
+    path('<slug:slug>', SingleBulletinView.as_view()),
+    path('<slug:slug>', ApproveBulletinView.as_view())
 ]
