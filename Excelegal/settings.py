c97971941f09f9a3dbfc9fd4cf2a5543f12d5bbb
ToxicityMax <ksingh2001.dev@gmail.com>
2022-05-06 22:00:37 +0530

    static root in settings

--- Excelegal/settings.py
+++ Excelegal/settings.py
@@ -167,6 +167,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'static'
+FORCE_SERVE_STATIC = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
